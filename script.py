0c50138b313327d8631770bfa0a5446280e352a9
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2020-09-03 15:36:07 +0200

    Done with plotting the infinity track

--- script.py
+++ script.py
@@ -5,20 +5,52 @@
 import rps.robotarium as robotarium
 import time
 import numpy as np
+import math
 
 running = True
 numberOfRobots = 1
 lineWidth = 1
 initialConditions = np.array(np.mat('0;0.00;3.1415'))
 r = robotarium.Robotarium(number_of_robots=numberOfRobots, show_figure=True, initial_conditions=initialConditions,sim_in_real_time=True)
-pathQuarter = [
-    [0,0,0.9],
-    [0,0.9,0.9]
-]
+
+
+def normalize(min, max, value): 
+    return min + (max - min) * value 
+
+def createCircle(x, y, fromRadians, toRadians, step, normalizeMin, normalizeMax): 
+    current = fromRadians
+    circle = [[],[]]
+    while(current < toRadians):
+        circle[0].append(x + normalize(normalizeMin, normalizeMax, math.cos(current)))
+        circle[1].append(y + normalize(normalizeMin, normalizeMax, math.sin(current)))
+        current += step
+
+    return circle
+
+
 def createFullPath(): 
     global path
-    path = pathQuarter
 
+    path = [
+        [0.0, 0.2, 1.0],
+        [0.0,-0.2,-0.2]
+    ]
+    c = createCircle(path[0][-1], path[1][-1] + 0.2, math.pi + (math.pi / 2.0), (2 * math.pi) + math.pi / 2.0, 0.1, 0, 0.2)
+    path[0].extend(c[0])
+    path[1].extend(c[1])
+    path[0].append(0.2)
+    path[1].append(0.2)
+    path[0].append(-0.2)
+    path[1].append(-0.2)
+    path[0].append(-1.0)
+    path[1].append(-0.2)
+    c2 = createCircle(path[0][-1], path[1][-1] + 0.2, (math.pi / 2.0), math.pi + (math.pi / 2.0), 0.1, 0, 0.2)
+    path[0].extend(reversed(c2[0]))
+    path[1].extend(reversed(c2[1]))
+    path[0].append(-0.2)
+    path[1].append(0.2)
+    path[0].append(0)
+    path[1].append(0)
 
 def plotTrack():
     r.axes.plot(path[0],path[1],color='red',linewidth = lineWidth)
